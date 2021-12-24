365a16d28da6bfe9442f3c496ff08ff14af3bb51
Steven Chu <steven.chu@keyxentic.com>
2021-12-24 11:23:13 +0800

    Fix mac error
    Fix the error of "is not a function"

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const os = require('os');
 if (os.platform() == 'win32') {
     detect = require('./src/detect_windows');
 } else {
-    detect = new Promise((resolve, reject) => {
-        reject("Not supported yet");
-    });
+    detect = function () {
+        return Promise.reject("Not supported yet");
+    }
 }
 
 module.exports = detect;
